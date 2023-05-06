805a83fef93fd497831abf3ded0e7ed5f690d622
Daniel Serrano <dserranog@unal.edu.co>
2023-05-06 11:32:10 -0500

    add delete funcionality

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -19,47 +19,64 @@ const createTodo = () => {
     isDone: false,
     isEditing: false,
   };
-  todoList.value = [newTodo, ...todoList.value];
+  todoList.value.push(newTodo);
+};
+
+const deleteTodo = (id: string) => {
+  todoList.value = todoList.value.filter((todo) => todo.id !== id);
 };
 </script>
 
 <template>
   <div
-    class="flex flex-col items-center justify-center flex-1 w-full h-full gap-8 my-10"
+    class="flex flex-col items-center justify-center flex-1 w-full h-full my-10"
   >
-    <div :key="todo.id" v-for="todo in todoList" class="flex flex-row gap-6">
-      <input
-        class="w-6"
-        v-model="todo.isDone"
-        type="checkbox"
-        :disabled="todo.isEditing"
-      />
-      <p
-        v-if="!todo.isEditing"
-        @click="todo.isEditing = todo.isDone ? false : !todo.isEditing"
+    <div v-if="todoList.length > 0">
+      <div
+        :key="todo.id"
+        v-for="todo in todoList"
+        class="flex flex-row gap-6 relative my-4"
       >
-        <s v-if="todo.isDone">
-          {{ todo.description }}
-        </s>
-        <span v-else>{{ todo.description }}</span>
-      </p>
-      <div v-else class="flex flex-row gap-4">
-        <input
-          type="text"
-          v-model="todo.description"
-          class="bg-gray-100 rounden-sm outline-none p-2"
-        />
         <button
-          class="text-sm rounded-sm bg-green-200 hover:bg-green-300 p-1"
-          @click="todo.isEditing = false"
+          class="px-3 absolute -left-16 top-1"
+          @click="deleteTodo(todo.id)"
         >
-          ok
+          <img src="../assets/x.svg" alt="" />
         </button>
+        <input
+          class="w-6"
+          v-model="todo.isDone"
+          type="checkbox"
+          :disabled="todo.isEditing"
+        />
+        <p
+          v-if="!todo.isEditing"
+          @click="todo.isEditing = todo.isDone ? false : !todo.isEditing"
+        >
+          <s v-if="todo.isDone">
+            {{ todo.description }}
+          </s>
+          <span v-else>{{ todo.description }}</span>
+        </p>
+        <div v-else class="flex flex-row gap-4">
+          <input
+            type="text"
+            v-model="todo.description"
+            class="bg-gray-100 rounden-sm outline-none p-2"
+          />
+          <button
+            class="text-sm rounded-sm bg-green-200 hover:bg-green-300 p-1"
+            @click="todo.isEditing = false"
+          >
+            ok
+          </button>
+        </div>
       </div>
     </div>
+    <div v-else>So empty...</div>
     <button
       @click="createTodo"
-      class="text-lg rounded-md bg-green-200 hover:bg-green-300 p-2"
+      class="text-lg rounded-md bg-green-200 hover:bg-green-300 p-2 my-6"
     >
       Add new
     </button>
